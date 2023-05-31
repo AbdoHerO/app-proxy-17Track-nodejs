54f30543798f95aeee73ec3a262a7ed26cfffeb1
AbdoHerO <abderrahmane.ouhna@gmail.com>
2023-06-01 05:16:39 +0800

    testing nodejs app

--- webhook.js
+++ webhook.js
@@ -19,23 +19,27 @@ app.use((req, res, next) => {
     next();
   });
 
+
+  let trackingData = null;
+
+  app.post('/webhook/17track', (req, res) => {
+    // Process and extract the tracking data from the webhook payload
+    const { data } = req.body;
   
-app.all('/webhook/17track', (req, res) => {
-    if (req.method === 'GET') {
-      // Handle GET request
-      console.log('METHODE', "GET");
-      res.sendStatus(200); // Respond with a success status code or any desired response
-    } else if (req.method === 'POST') {
-      // Handle POST request
-      const payload = req.body; // Access the payload data sent by 17TRACK
-      console.log('Webhook payload:', payload); // Log the payload for testing
+    // Store the tracking data temporarily
+    trackingData = data;
   
-      console.log('METHODE', "POST");
-      // Process the payload and perform any necessary actions
+    // Respond with a success message
+    res.status(200).json({ message: 'Webhook event received and data stored.' });
+  });
   
-      res.sendStatus(200); // Respond with a success status code
+  app.get('/tracking-data', (req, res) => {
+    if (trackingData) {
+      // Return the stored tracking data
+      res.status(200).json(trackingData);
     } else {
-      res.sendStatus(405); // Respond with a "Method Not Allowed" status code for other methods
+      // Return an empty response or an appropriate error message
+      res.status(404).json({ error: 'Tracking data not found.' });
     }
   });
 
@@ -43,11 +47,3 @@ const port = 3000; // Specify the port number
 app.listen(port, () => {
   console.log(`Webhook server is running on port ${port}`);
 });
-  
-// var http = require('http') ; 
-// function onRequest( request, response) {
-//      response.writeHead(200, {'Content-Type': 'text/plain'}); 
-//      response.write('Hello World'); 
-//      response. end(); 
-// }
-// http.createServer(onRequest).listen(8000); 
