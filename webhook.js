55db251e8ef9ebfeffe1d4ef81b6261515037813
AbdoHerO <abderrahmane.ouhna@gmail.com>
2023-06-01 05:55:43 +0800

    testing nodejs app

--- webhook.js
+++ webhook.js
@@ -19,20 +19,26 @@ app.use((req, res, next) => {
     next();
   });
 
-
-  let trackingData = null;
-
-  app.post('/webhook/17track', (req, res) => {
-    // Process and extract the tracking data from the webhook payload
-    const { data } = req.body;
   
-    // Store the tracking data temporarily
-    trackingData = data;
+app.all('/webhook/17track', (req, res) => {
+    if (req.method === 'GET') {
+      // Handle GET request
+      console.log('METHODE', "GET");
+      res.sendStatus(200); // Respond with a success status code or any desired response
+    } else if (req.method === 'POST') {
+      // Handle POST request
+      const payload = req.body; // Access the payload data sent by 17TRACK
+      console.log('Webhook payload:', payload); // Log the payload for testing
   
-    // Respond with a success message
-    res.status(200).json({ message: 'Webhook event received and data stored.' });
+      console.log('METHODE', "POST");
+      // Process the payload and perform any necessary actions
+  
+      res.sendStatus(200); // Respond with a success status code
+    } else {
+      res.sendStatus(405); // Respond with a "Method Not Allowed" status code for other methods
+    }
   });
-  
+
   app.get('/tracking-data', (req, res) => {
     if (trackingData) {
       // Return the stored tracking data
@@ -42,8 +48,15 @@ app.use((req, res, next) => {
       res.status(404).json({ error: 'Tracking data not found.' });
     }
   });
-
 const port = 3000; // Specify the port number
 app.listen(port, () => {
   console.log(`Webhook server is running on port ${port}`);
 });
+  
+// var http = require('http') ; 
+// function onRequest( request, response) {
+//      response.writeHead(200, {'Content-Type': 'text/plain'}); 
+//      response.write('Hello World'); 
+//      response. end(); 
+// }
+// http.createServer(onRequest).listen(8000); 
